a78b79ec12d08a4812e03e2d1c531c7643e9b948
Ljabert <contact@ljabert.com>
2020-11-12 00:16:16 +0100

    houlien mit o

--- mc-mods-setup.bat
+++ mc-mods-setup.bat
@@ -3,7 +3,7 @@
 RD /S /Q %temp%\McHouliensohnMods
 mkdir %temp%\McHouliensohnMods
 
-git clone https://github.com/Ljabert/mc-huliensohn-mods %temp%\McHouliensohnMods
+git clone https://github.com/Ljabert/mc-houliensohn-mods %temp%\McHouliensohnMods
 
 RD /S /Q %appdata%\.minecraft\.git
 RD /S /Q %appdata%\.minecraft\mods
@@ -13,7 +13,7 @@ RD /S /Q %temp%\McHouliensohnMods\.git
 
 move /Y %temp%\McHouliensohnMods\config 	%appdata%\.minecraft
 move /Y %temp%\McHouliensohnMods\mods 		%appdata%\.minecraft
-move /Y %temp%\McHouliensohnMods\* 			%appdata%\.minecraft
+move /Y %temp%\McHouliensohnMods\* 			  %appdata%\.minecraft
 
 RD /S /Q %temp%\McHouliensohnMods
 
