19025c424bbaa51fafbce5049bb0ba3ceac122c5
Ljabert <contact@ljabert.com>
2020-11-11 17:55:16 +0100

    Added batch script to clone the repo in .minecraft

---
+++ mc-mods-setup.bat
@@ -0,0 +1,22 @@
+@echo off
+
+RD /S /Q %temp%\McHouliensohnMods
+mkdir %temp%\McHouliensohnMods
+
+git clone https://github.com/Ljabert/mc-huliensohn-mods %temp%\McHouliensohnMods
+
+RD /S /Q %appdata%\.minecraft\.git
+RD /S /Q %appdata%\.minecraft\config
+RD /S /Q %appdata%\.minecraft\mods
+
+xcopy /H /S /E /I /Y %temp%\McHouliensohnMods\.git 	%appdata%\.minecraft\.git
+RD /S /Q %temp%\McHouliensohnMods\.git
+
+move /Y %temp%\McHouliensohnMods\config 	%appdata%\.minecraft
+move /Y %temp%\McHouliensohnMods\mods 		%appdata%\.minecraft
+move /Y %temp%\McHouliensohnMods\* 			%appdata%\.minecraft
+
+RD /S /Q %temp%\McHouliensohnMods
+
+echo Fertig
+pause
