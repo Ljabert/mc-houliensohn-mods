58f130a6ac3dd51c6204f6cec70c4e6e78acdcce
Ljabert <contact@ljabert.com>
2021-03-09 22:12:30 +0100

    Neue Mods (dieses mal wirklich)

--- mc-mods-update.bat
+++ mc-mods-update.bat
@@ -10,6 +10,10 @@ IF EXIST .git (
 	git clean -fd
 	cd ..
 	git pull
+	
+	REM 100MB Limit umgehen
+	type .\mods\TheBetweenlands.jar.1 .\mods\TheBetweenlands.jar.2 > .\mods\TheBetweenlands.jar
+	
 	pause
 ) ELSE (
 	echo Kein Git-Repo gefunden, bitte "mc-mods-setup.bat" ausfuehren.
